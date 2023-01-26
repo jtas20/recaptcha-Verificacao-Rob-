2e5391a6676d1efa711ee015976f9f32eaa6a4f0
Tiago Angelo <thiagoangelos2020@gmail.com>
2023-01-26 14:13:29 -0300

    48459353

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
-var submitBtn = document.querySelector('#submitBtn');
+var botao = document.querySelector('#botao');
 
-function recaptchaCallback()
-{
-   jQuery('#submitBtn').prop('disabled', false);
+function VerifyHuman(){
+    var response = grecaptcha.getResponse();
+
+    if(response.length == 0){
+        var botao = document.querySelector('#botao');
+        botao.textContent = 'erro';
+    }else{
+        var botao = document.querySelector('#botao');
+        botao.textContent = 'Sucesso';
+    }
+
 }
