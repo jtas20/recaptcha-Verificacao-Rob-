aff8ce33cfa51ff9599bb2df29aacd9749558392
Tiago Angelo <thiagoangelos2020@gmail.com>
2023-01-26 14:32:40 -0300

    4343

--- js/index.js
+++ js/index.js
@@ -1,14 +1,3 @@
-var botao = document.querySelector('#botao');
-
 function recaptchaCallback(){
-    var response = grecaptcha.getResponse();
-
-    if(response.length == 0){
-        var botao = document.querySelector('#botao');
-        botao.textContent = 'erro';
-    }else{
-        var botao = document.querySelector('#botao');
-        botao.textContent = 'Sucesso';
-    }
-
+    alert('Caixa Marcada!');
 }
