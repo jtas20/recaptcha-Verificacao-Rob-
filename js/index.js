ff35dee6682b6be420de64c4b3dcdfc873d22b4b
Tiago Angelo <thiagoangelos2020@gmail.com>
2023-01-26 15:02:49 -0300

    019

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 function recaptchaCallback(){
     let btnClick = document.getElementById("botao");
-    btnClick.disabled = true;
-    btnClick.textContent = 'HIBILITADO';
+    btnClick.style.display = block;
 }
